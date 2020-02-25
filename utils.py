7d2db85488d10d4fab89edb8d6e8f950d96ce829
mnicolas94 <miguelcok27@gmail.com>
2020-02-25 15:58:13 -0500

    Removed unused import statements in utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,7 @@
-import imutils.paths
 import random
 from timeit import default_timer as timer
 import cv2 as cv
-import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
-import re
 import os
 from os import path
 from datetime import datetime
