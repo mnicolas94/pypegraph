6464592e09ce2a8d4d6fe8f6c371e7e579f934dd
mnicolas94 <miguelcok27@gmail.com>
2022-04-07 15:02:08 -0400

    BUILD: update setup.py and publish_on_pypi.yml to support better version bumping strategy

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 import setuptools
+from setuptools import find_packages
 import subprocess
 import sys
+
+
+def exists_version():
+    bash_command = "git describe --tags --abbrev=0 --match v[0-9]*"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    outputstr = str(output)
+    outputstr = outputstr[3:-3]  # "b'vXX.XX.XX\\n'" becomes "XX.XX.XX"
+    outputstr = outputstr.replace('-', '.')
+    exists = outputstr != ""
+    return exists
 
 
 def get_last_version():
@@ -11,12 +23,20 @@ def get_last_version():
     outputstr = outputstr[3:-3]  # "b'vXX.XX.XX\\n'" becomes "XX.XX.XX"
     outputstr = outputstr.replace('-', '.')
     major, minor, patch, *_ = outputstr.split('.')
-    version = f'{major}.{minor}.{patch}'
-    return version
+    ver = f'{major}.{minor}.{patch}'
+    return ver
 
 
-def bump_patch(version: str):
-    major, minor, patch = version.split('.')
+def current_commit_has_tag():
+    bash_command = "git describe --tags --exact-match HEAD"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    has_tag = output != b""
+    return has_tag
+
+
+def bump_patch(ver: str):
+    major, minor, patch = ver.split('.')
     bumped = str(int(patch) + 1)
     return f'{major}.{minor}.{bumped}'
 
@@ -31,11 +51,15 @@ def push_new_version_as_tag(version):
 
 
 if __name__ == '__main__':
-    last_version = get_last_version()
-    version = bump_patch(last_version)
+    exists = exists_version()
+    version = get_last_version() if exists else "0.1.0"
+    already_has_version = current_commit_has_tag()
+    if not already_has_version and exists:
+        version = bump_patch(version)
 
     if sys.argv[1] == 'pushtag':
-        push_new_version_as_tag(version)
+        if not already_has_version:
+            push_new_version_as_tag(version)
     else:
         with open("README.md", "r") as fh:
             long_description = fh.read()
@@ -49,7 +73,7 @@ if __name__ == '__main__':
             long_description=long_description,
             long_description_content_type="text/markdown",
             url="https://github.com/mnicolas94/pypegraph",
-            packages=['pypegraph'],
+            packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
             classifiers=[
                 "Programming Language :: Python :: 3",
                 "License :: OSI Approved :: MIT License",
